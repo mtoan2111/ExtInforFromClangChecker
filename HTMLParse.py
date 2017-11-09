95830eb7b1db8a9f0d19bee223c75339b7f31269
mtoan2111 <toan93.hust@gmail.com>
2017-11-09 23:07:39 +0800

    fixing bugs

--- HTMLParse.py
+++ HTMLParse.py
@@ -7,10 +7,10 @@ class HtmlParse:
 
   SoureFile = ""
   Output = ""
-  Lst_Dangerous = []
-  hMap_Files = {}
   WorkingDir = ""
   Soup = BeautifulSoup
+  Lst_Dangerous = list()
+  hMap_Files = dict()
 
   def __init__(self, SoureFile, Output, ListDangerous):
     self.SoureFile = SoureFile
@@ -23,19 +23,19 @@ class HtmlParse:
 
   def analyzingSoureFile(self):
     if len(self.Lst_Dangerous) > 0:
-      for danger in self.Lst_Dangerous:
-        for item in self.Soup.findAll("tr",{"class": danger}):
-          td_Tag = item.findAll('td')
-          self.hMap_Files[int(td_Tag[4].text)] = self.WorkingDir + td_Tag[2].text
-              # self.writeToFile(allTag[2].text, allTag[4].text)
+      for item in self.Soup.findAll("tr",{"class": self.Lst_Dangerous}):
+        td_Tag = item.findAll('td')
+        self.hMap_Files[int(td_Tag[4].text)] = self.WorkingDir + td_Tag[2].text
       for key, value in sorted(self.hMap_Files.iteritems(), key=lambda (k, v): (v, k)):
         self.writeToFile(self.hMap_Files[key], key);
+      print ("Analyzing success fully, Ouput file dir:",self.Output )
+    else:
+      print ("Oops: doesn't have any warning that you want")
 
   def getWorkingDir(self):
-  #find first table for collecting information
-    temp = self.Soup.find("table")
-    allTag = temp.findAll('td')
-    self.WorkingDir = allTag[1].text + "/"
+    infoTable = self.Soup.find("table")
+    th_Tag = infoTable.findAll('td')
+    self.WorkingDir = th_Tag[1].text + "/"
 
   def rmOutputFile(self):
     try:
