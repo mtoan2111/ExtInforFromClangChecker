2d0333713b2c835d4a8e335c3f719649b57210b7
toannm <toan93.hust@gmail.com>
2017-11-21 14:21:23 +0800

    Given input dir

--- ExtDangerousFile.py
+++ ExtDangerousFile.py
@@ -1,12 +1,22 @@
 from __future__ import print_function
 from HTMLParse import HtmlParse
+import sys, os, datetime
 
-list_dangerous = []
+class Analyzing:
+  list_dangerous = []
+  test = HtmlParse
+  def __init__(self):
+    self.list_dangerous.append("bt_security_potential_insecure_memory_buffer_bounds_restriction_in_call_strcpy_")
+    self.list_dangerous.append("bt_security_potential_insecure_memory_buffer_bounds_restriction_in_call_strcat_")
+    self.list_dangerous.append("bt_logic_error_out-of-bound_array_access")
+    if len(sys.argv) < 2:
+      print ("Oops: Missing input direction")
+      return
+    if os.path.isdir(sys.argv[1]) == 0:
+      print ("Oops: Input does not dir")
+      return
+    time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    Html = HtmlParse(sys.argv[1] + "/index.html",sys.argv[1] +"/" + time + ".txt", self.list_dangerous)
+    Html.analyzingSoureFile()
 
-list_dangerous.append("bt_security_potential_insecure_memory_buffer_bounds_restriction_in_call_strcpy_")
-list_dangerous.append("bt_security_potential_insecure_memory_buffer_bounds_restriction_in_call_strcat_")
-list_dangerous.append("bt_logic_error_out-of-bound_array_access")
-
-
-test = HtmlParse("/home/toannm/report/02_11/less/ffmpeg/2017-11-03-152218-9185-1/index.html","/home/toannm/dangerous/readelf.txt", list_dangerous)
-test.analyzingSoureFile()
+test = Analyzing()
--- HTMLParse.py
+++ HTMLParse.py
@@ -12,6 +12,9 @@ class HtmlParse:
   hMap_Files = dict()
 
   def __init__(self, SoureFile, Output, ListDangerous):
+    if os.path.exists(SoureFile) == 0:
+      print ("Oops: Missing HTML file in dir")
+      return
     self.SoureFile = SoureFile
     self.Output = Output
     self.Lst_Dangerous = deepcopy(ListDangerous)
@@ -21,18 +24,20 @@ class HtmlParse:
 
   def analyzingSoureFile(self):
     if len(self.Lst_Dangerous) > 0:
-      try:
-        os.remove(self.Output)
-      except OSError as e:
-        if e.errno != errno.ENOENT:  # erro.ENOENT = no such file or directory
-          raise
+      if os.path.isfile(self.Output):
+        try:
+          os.remove(self.Output)
+        except OSError as e:
+          if e.errno != errno.ENOENT:  # erro.ENOENT = no such file or directory
+            raise
       with open(self.Output, "a+") as fp:
         for item in self.Soup.findAll("tr",{"class": self.Lst_Dangerous}):
           td_Tag = item.findAll('td')
           self.hMap_Files[int(td_Tag[4].text)] = self.WorkingDir + td_Tag[2].text
         for key, value in sorted(self.hMap_Files.iteritems(), key=lambda (k, v): (v, k)):
           fp.writelines(self.hMap_Files[key] + ": " + str(key) + "\n");
-        print ("Analyzing success fully, Ouput file dir:",self.Output )
+        print ("Analyzing success fully")
+        print ("Ouput file dir:",self.Output )
     else:
       print ("Oops: doesn't have any warning that you want")
 
